544734142124f250654063c3a1d2c89502a7de50
Amit <amitkuswain6@gmail.com>
2024-09-24 14:41:53 +0530

    for small project

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./App.css";
+import MsgBox from "./MsgBox";
+
+
+function App() {
+  return (
+    <>
+    <MsgBox userName="Amit swain" textColor="yellow"/>
+    <MsgBox userName="lilu" textColor="green"/>
+    <MsgBox userName="Rakesh" textColor="blue"/>
+    </>
+  );
+}
+
+export default App
